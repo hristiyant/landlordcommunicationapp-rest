99e52d2c3723c36af887218ac18e435c7fa2988b
vladimirVenkov <vladimir.ognyanov.venkov@gmail.com>
2018-10-15 14:36:40 +0300

    Initial commit

---
+++ src/main/java/com/spiderman/landlordcommunicationapp/LandlordcommunicationappApplication.java
@@ -0,0 +1,12 @@
+package com.spiderman.landlordcommunicationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandlordcommunicationappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LandlordcommunicationappApplication.class, args);
+    }
+}
