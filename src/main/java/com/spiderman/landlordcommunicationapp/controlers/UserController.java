982db45b49f035a2b576d1c83e29d7649703608c
Hristiyan Todorov <hristiyant@gmail.com>
2018-10-22 17:44:19 +0300

    Add UserController.

---
+++ src/main/java/com/spiderman/landlordcommunicationapp/controlers/UserController.java
@@ -0,0 +1,43 @@
+package com.spiderman.landlordcommunicationapp.controlers;
+
+import com.spiderman.landlordcommunicationapp.models.Rating;
+import com.spiderman.landlordcommunicationapp.models.User;
+import com.spiderman.landlordcommunicationapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Users")
+@PreAuthorize("isAnonymous()")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAll();
+    }
+
+    @GetMapping("/new")
+    public User saveUser(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @GetMapping("/rateUser")
+    public User rateUser(@RequestBody User user, @RequestBody double rating) {
+        return userService.rateUser(user, rating);
+    }
+
+
+}
